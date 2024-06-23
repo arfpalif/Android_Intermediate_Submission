b77df5391027727a8e6cf3a23c98cb0e683d63d8
arfpalif <alifraihanfirmanp@gmail.com>
2024-06-23 22:19:31 +0700

    final

---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/StoryPagingSource.kt
@@ -0,0 +1,37 @@
+package com.slavouz.storyapp.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.slavouz.storyapp.data.response.ListStoryItem
+import com.slavouz.storyapp.data.retrofit.ApiService
+import retrofit2.await
+
+class StoryPagingSource(private val apiService: ApiService) : PagingSource<Int, ListStoryItem>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ListStoryItem> {
+        return try {
+            val page = params.key ?: INITIAL_PAGE_INDEX
+            val responseData = apiService.getStories(page, params.loadSize)
+            val storyItems = responseData.await().listStory
+
+            LoadResult.Page(
+                data = storyItems,
+                prevKey = if (page == INITIAL_PAGE_INDEX) null else page - 1,
+                nextKey = if (responseData == null) null else page + 1
+            )
+        } catch (exception: Exception) {
+            return LoadResult.Error(exception)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, ListStoryItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    private companion object {
+        const val INITIAL_PAGE_INDEX = 1
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/uploadStory/UploadStoryActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.loginwithanimation.view.UploadStory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.loginwithanimation.R
+
+class UploadStoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_story2)
+    }
+}
